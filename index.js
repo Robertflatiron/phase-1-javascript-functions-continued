e287560da707258bc549555f7114115120a6d340
Robert Gaspar <gaspar.robert7@gmail.com>
2021-11-02 19:42:20 -0500

    commit tree e80935bb5ce733d04a322cffa5337af43150f02b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork();
+
+function wrapAdjective(part1 = "*") {
+    return function(abc = "special") {
+        return `You are ${part1}${abc}${part1}!`
+    }
+}
